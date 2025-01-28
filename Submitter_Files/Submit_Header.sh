dee0ca537e896d685c37f4b683143d5e485711f1
Marco Cappelletti <79407298+cappecaps@users.noreply.github.com>
2025-01-28 14:35:15 +0100

    Update Submit_Header.sh

--- Submitter_Files/Submit_Header.sh
+++ Submitter_Files/Submit_Header.sh
@@ -18,29 +18,25 @@
 StartTime=$(date +%s.%N)
 printf ">>>>>>>>>>>> Calculation start at $(date)\n\n"
 
-path=$(echo $SLURM_SUBMIT_DIR | cut -d '/' -f 6-20)
+path=$(echo $SLURM_SUBMIT_DIR | cut -d '/' -f 6-30)
 
-if [ ${#PWD} -gt 100 ]; then
-        firstdir=$(echo $path | cut -d '/' -f 1)
-        lastNdir=$(echo $path | rev | cut -d '/' -f 1-5 | rev)
-        path="$firstdir"/.../"$lastNdir"
+if [ ${#PWD} -gt 110 ]; then
+	Ndir=$(echo $path | awk -F '/' '{print NF}')
+	firstdir=$(echo $path | cut -d '/' -f 1)
+	if [ $Ndir -ge 6 ]; then
+		lastNdir=$(echo $path | rev | cut -d '/' -f 1-$((Ndir-3)) | rev)
+		path="$firstdir"/.../"$lastNdir"
+	elif [ $Ndir -ge 3 ]; then
+		lastNdir=$(echo $path | rev | cut -d '/' -f 1-$((Ndir-2)) | rev)
+		path="$firstdir"/.../"$lastNdir"
+	fi
 fi
 
 printf "$SLURM_JOB_ID\t$SLURM_JOB_NAME\t$(date +%F)\t$path\n" >> ~/bin/QueueHistory/RunningJobs.txt
-printf "$SLURM_JOB_ID\t$SLURM_JOB_NAME\t$(date +%F)\t$path\n" >> ~/bin/QueueHistory/JobsBackup.txt
+
+PJFline=$(awk '/'$SLURM_JOB_NAME'/{print NR}' ~/bin/QueueHistory/PendingJobs.txt | head -n1)
+sed -i "${PJFline}d" ~/bin/QueueHistory/PendingJobs.txt
 
 part=$(echo $HOSTNAME | sed 's/[0-9]*//g ; s/-//g')
 
-#if [ $part == "veralux" ] ; then
-#  module unuse /apps/Vera/modules/all/Core
-#  module use /apps/Hebbe/modules/all/Core
-#  module use /apps/Hebbe7/modules/all/Core
-#  echo "Cluster: lux"
-#elif [ $part == "vera" ] ; then
-#  echo "Cluster: vera"
-#else
-#  echo "something wrong happened"
-#fi
-
 echo ""
-
